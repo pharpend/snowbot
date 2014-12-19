c346b2887baa26263e66e64fa1df3d2716acd1d8
Denis Kasak <dkasak@termina.org.uk>
2014-12-19 12:48:45 +0100

    Implement memos and channel commands.
    This implements the ability for users to leave memos to other users, to
    be delivered via a private message when the recipient next joins the
    channel. Afterwards, the memos are cleared from the database.
    
    The parser was changed a bit to accomodate commands with arguments.
    Also, since users usually expect to be able to leave memos from the
    channel, the bot can now be given commands by highlighting it in the
    channel (messages are considered to highlight if they mention the bot's
    nickname as the first word of the message, optionally followed by some
    suffix, currently ":" or ","), followed by the command. The bot sends
    its response to wherever it received the command from (i.e. either the
    channel or via a private message to the user), except in the case of
    the known_users command where the output is inappropriately long.

--- Main.hs
+++ Main.hs
@@ -7,7 +7,7 @@ import qualified Network.IRC as IRC
 import Data.Set as S
 import Data.Map as M
 import Data.List as L
-import Data.Maybe as L (mapMaybe)
+import Data.Maybe as L (mapMaybe, maybeToList)
 import Data.ByteString.Char8 as BSC
 
 import Data.Time.Clock (UTCTime, getCurrentTime)
@@ -54,13 +54,21 @@ defaultUserPrefs = UserPrefs { upDoLog = False }
 data BotState = BotState
                     { bsLog :: Map UTCTime TimeSlice
                     , bsUserPrefs :: Map UserName UserPrefs
+                    , bsMemos :: Map UserName [Memo]
                     } deriving (Typeable)
+
+data Memo = Memo
+                { memoTime :: UTCTime
+                , memoSender :: UserName
+                , memoContent :: ByteString
+                } deriving (Show, Typeable)
 
 $(deriveSafeCopy 0 'base ''UserPrefs)
 $(deriveSafeCopy 0 'base ''IRC.Prefix)
 $(deriveSafeCopy 0 'base ''Message)
 $(deriveSafeCopy 0 'base ''TimeSlice)
 $(deriveSafeCopy 0 'base ''BotState)
+$(deriveSafeCopy 0 'base ''Memo)
 
 instance Monoid TimeSlice where
     mappend a b = TimeSlice
@@ -80,24 +88,41 @@ getUserPrefs user = ask >>= return . M.lookup user . bsUserPrefs
 getAllUserPrefs :: Query BotState (Map UserName UserPrefs)
 getAllUserPrefs = ask >>= return . bsUserPrefs
 
+getMemos :: UserName -> Query BotState [Memo]
+getMemos user = ask >>= return . join . L.maybeToList . M.lookup user . bsMemos
+
 setDoLog :: UserName -> Bool -> Update BotState ()
-setDoLog user do_log = modify $ \ (BotState log prefs) -> BotState log $ M.insertWith (\ _ prefs -> prefs { upDoLog = do_log }) user (defaultUserPrefs { upDoLog = do_log }) prefs
+setDoLog user do_log = modify $ \ (BotState log prefs memos) -> BotState log (M.insertWith (\ _ prefs -> prefs { upDoLog = do_log }) user (defaultUserPrefs { upDoLog = do_log }) prefs) memos
 
 logMessage :: UTCTime -> Message -> Update BotState ()
-logMessage time message = modify $ \ (BotState log users) -> BotState (M.insertWith mappend time (TimeSlice S.empty S.empty [message]) log) users
+logMessage time message = modify $ \ (BotState log users memos) -> BotState (M.insertWith mappend time (TimeSlice S.empty S.empty [message]) log) users memos
 
 logArrival :: UTCTime -> UserName -> Update BotState ()
-logArrival time user = modify $ \ (BotState log users) -> BotState (M.insertWith mappend time (TimeSlice (S.singleton user) S.empty []) log) (M.insertWith (flip const) user defaultUserPrefs users)
+logArrival time user = modify $ \ (BotState log users memos) -> BotState (M.insertWith mappend time (TimeSlice (S.singleton user) S.empty []) log) (M.insertWith (flip const) user defaultUserPrefs users) memos
 
 logDeparture :: UTCTime -> UserName -> Update BotState ()
-logDeparture time user = modify $ \ (BotState log users) -> BotState (M.insertWith mappend time (TimeSlice S.empty (S.singleton user) []) log) users
+logDeparture time user = modify $ \ (BotState log users memos) -> BotState (M.insertWith mappend time (TimeSlice S.empty (S.singleton user) []) log) users memos
+
+recordMemo :: UserName -> UserName -> UTCTime -> ByteString -> Update BotState ()
+recordMemo sender recipient time content = modify $ \ (BotState log users memos) ->
+                                    let appendMemo :: Memo -> Maybe [Memo] -> Maybe [Memo]
+                                        appendMemo m (Just ms) = Just $ m:ms
+                                        appendMemo m Nothing = Just [m]
+
+                                        memo = Memo time sender content
+                                        memos' = M.insertWith mappend recipient [memo] memos
+                                    in BotState log users memos'
+
+clearMemos :: UserName -> Update BotState ()
+clearMemos user = modify $ \ (BotState l u _) -> BotState l u M.empty
 
 forgetUser :: UserName -> Update BotState ()
-forgetUser user = modify $ \ (BotState log users) -> BotState log $ M.delete user users
+forgetUser user = modify $ \ (BotState log users memos) -> BotState log (M.delete user users) memos
 
 $(makeAcidic ''BotState [ 'getLog, 'getAllUserPrefs, 'getUserPrefs
-                        , 'setDoLog , 'logMessage, 'logArrival, 'logDeparture
+                        , 'setDoLog, 'logMessage, 'logArrival, 'logDeparture
                         , 'forgetUser
+                        , 'getMemos, 'recordMemo, 'clearMemos
                         ])
 
 greeting :: [ByteString]
@@ -115,6 +140,12 @@ greeting =
     , "*"
     , BSC.replicate 60 '*'
     ]
+
+memoToMessageFor :: UserName -> Memo -> Message
+memoToMessageFor recipient (Memo time sender content) = IRC.privmsg recipient text
+    where time' = BSC.pack (show time)
+          sender' = "<" <> sender <> "> "
+          text = time' <> ": " <> sender' <> content
 
 logPart :: AcidState BotState -> BotPartT IO ()
 logPart database = do
@@ -135,7 +166,14 @@ logPart database = do
             guard $ user /= nickName <> "-devel"
 
             prefs <- query' database $ GetUserPrefs user
-            log <- query' database GetLog
+            log   <- query' database GetLog
+            memos <- query' database $ GetMemos user
+
+            sendMessage $ case memos of
+                [] -> IRC.privmsg user "no memos"
+                _  -> IRC.privmsg user "some memos were left for you:"
+            forM_ (L.reverse memos) (sendMessage . memoToMessageFor user)
+            update' database $ ClearMemos user
 
             case prefs of
                 Just x | upDoLog x -> do
@@ -150,9 +188,9 @@ logPart database = do
                             [] -> [ "no missed messages" ]
                             _ -> "you missed the following while away: " : L.reverse (L.mapMaybe (uncurry renderMessage) messages)
 
-                    forM_ msg_list $ sendMessage . IRC.Message Nothing "PRIVMSG" . (user:) . (:[])
+                    forM_ msg_list $ sendMessage . IRC.privmsg user
 
-                Nothing -> forM_ greeting $ sendMessage . IRC.Message Nothing "PRIVMSG" . (user:) . (:[])
+                Nothing -> forM_ greeting $ sendMessage . IRC.privmsg user
 
                 _ -> return ()
 
@@ -165,24 +203,59 @@ logPart database = do
             IRC.NickName user _ _ <- maybeZero $ IRC.msg_prefix message
             let channel : msg : _ = IRC.msg_params message
 
-            case IRC.msg_params message of
-                (c : _) | c == botChannel -> update' database $ LogMessage time message
+            when (channel == botChannel) $
+                update' database (LogMessage time message)
 
-                [ _, "log" ] -> do
+            let highlights :: [ByteString] -> ByteString -> Bool
+                highlights (x:_) nick = L.any (== x) validHighlights
+                    where suffixes = ["", ":", ","]
+                          validHighlights = L.map (nick <>) suffixes
+                highlights [] _ = False
+
+                msg' = BSC.split ' ' msg
+                isPrivateMessage = channel == nickName
+                isCommand = (channel == botChannel && msg' `highlights` nickName) || isPrivateMessage
+
+            guard isCommand
+
+            let command = if isPrivateMessage
+                             then msg'
+                             else L.tail msg'
+
+            case command of
+                ["log"] -> do
                     update' database $ SetDoLog user True
-                    sendMessage $ IRC.Message Nothing "PRIVMSG" [ user, "when you log in, you'll be sent messages since you last left" ]
+                    if isPrivateMessage
+                        then sendMessage $ IRC.privmsg user "when you log in, you'll be sent messages since you last left"
+                        else sendMessage $ IRC.privmsg channel (user <> ", when you log in, you'll be sent messages since you last left")
 
-                [ _, "nolog" ] -> do
+                ["nolog"]  -> do
                     update' database $ SetDoLog user False
-                    sendMessage $ IRC.Message Nothing "PRIVMSG" [ user, "you will no longer be sent messages on your return" ]
+                    if isPrivateMessage
+                        then sendMessage $ IRC.privmsg user "you will no longer be sent messages on your return"
+                        else sendMessage $ IRC.privmsg channel (user <> ", you will no longer be sent messages on your return")
 
-                [ _, "forget" ] -> update' database $ ForgetUser user
+                ["forget"] -> update' database $ ForgetUser user
 
-                [ _, "known_users" ] -> do
+                ["known_users"] -> do
                     prefs <- query' database GetAllUserPrefs
-                    forM_ (M.toAscList prefs) $ \ (u, p) -> sendMessage $ IRC.Message Nothing "PRIVMSG" [user, u <> ": " <> BSC.pack (show p)]
+                    forM_ (M.toAscList prefs) $ \ (u, p) -> sendMessage $ IRC.privmsg user (u <> ": " <> BSC.pack (show p))
 
-                _ -> sendMessage $ IRC.Message Nothing "PRIVMSG" [user, "what?"]
+                ("memo":recipient:content) | not $ L.null content -> do
+                    let content' = BSC.concat (L.intersperse " " content)
+                    logM Normal $ "Recorded memo from " <> user <> " for " <> recipient <> " with content \"" <> content' <> "\""
+                    update' database $ RecordMemo user recipient time content'
+                    if isPrivateMessage
+                       then sendMessage $ IRC.privmsg user "Memo recorded."
+                       else sendMessage $ IRC.privmsg channel (user <> ", memo recorded.")
+
+                -- unknown command
+                (n:_) | isPrivateMessage -> sendMessage $ IRC.privmsg user "what?"
+                      | otherwise -> sendMessage $ IRC.privmsg channel (user <> ", what?")
+
+                -- empty command (happens when you just highlight the bot in the channel)
+                _ | isPrivateMessage -> sendMessage $ IRC.privmsg user "what?"
+                  | otherwise -> sendMessage $ IRC.privmsg channel (user <> ", what?")
 
     case IRC.msg_command message of
         "PRIVMSG" -> messaged
@@ -191,9 +264,8 @@ logPart database = do
         "JOIN" -> arriving
         cmd -> logM Normal $ BSC.pack (show time) <> ": unrecognized command: " <> cmd
 
-
 main = do
-    database <- openLocalState (BotState M.empty M.empty)
+    database <- openLocalState (BotState M.empty M.empty M.empty)
 
     let channels = S.singleton botChannel
         config = nullBotConf
